ad3169e4f147b626efec335d86dd2000a9c7931e
Marcos L <lopezm@zippyttech.com>
2022-10-06 19:26:10 -0400

    implementando aditable en modelo

--- app/Models/DateTemplate.php
+++ app/Models/DateTemplate.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class DateTemplate extends Model
+class DateTemplate extends Model implements Auditable
 {
     use
         HasFactory,
