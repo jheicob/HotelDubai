46554491a9ea0e1df7ad7d45bbc462a6caab2a4f
Marcos L <lopezm@zippyttech.com>
2022-10-08 10:31:52 -0400

    cambiando larvel mix por laravel vite

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,7 +1,10 @@
-window._ = require('lodash');
+// window._ = require('lodash');
+import _ from 'lodash'
 
 try {
-    require('bootstrap');
+    // import 'bootstrap'
+
+    // require('bootstrap');
 } catch (e) {}
 
 /**
@@ -9,8 +12,8 @@ try {
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
-window.axios = require('axios');
+import axios from 'axios'
+window.axios = axios
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
@@ -26,7 +29,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
